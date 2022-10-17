f1aa1c1d1de7a71b031faa9dfec09f5a614e492c
Bruno PC <blgianini@gmail.com>
2022-10-17 20:28:44 -0300

    new port

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ app.get("/update/:id", async (req, res)=> {
     }
 })
 
-app.listen(5000, () => {console.log("Express server listening on port 5000")});
+app.listen(process.env.PORT || 5000, () => {console.log("Express server listening on port 5000")});
